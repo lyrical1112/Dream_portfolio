db1716d67931753085920966c887f9cc44b3eb21
lyrical1112 <lyrical1112@naver.com>
2022-02-28 14:38:27 +0900

    JS: Navbar menu click
    바닐라 스크립트를 활용해 네브바 스크롤링 기능 추가

--- js/main.js
+++ js/main.js
@@ -12,3 +12,17 @@ document.addEventListener ('scroll', () =>{
         navbar.classList.remove('navbar--dark')
     }
 });
+
+// navbar 메뉴 클릭시 스크롤링
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=> {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+
+});
